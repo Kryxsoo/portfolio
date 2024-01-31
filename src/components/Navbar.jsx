c03d452f3651732cda6357c6092c3d2da0290ec5
Mathew Charm <mattcharmz@gmail.com>
2024-01-30 18:46:41 -0800

    layout of App.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { link } from 'react-router-dom';
+
+import { styles } from '../styles';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar

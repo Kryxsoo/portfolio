4ecb6f4988eaffa2696a6f333e718f2d638008c2
Mathew Charm <mattcharmz@gmail.com>
2024-02-07 03:14:35 -0800

    Swapped React-tilt with React Parallax-tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Tilt from 'react-tilt'
+import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
@@ -8,7 +8,7 @@ import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <tilt className="xs:w-[250px] w-full">
+    <Tilt className="xs:w-[250px] w-full">
     <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
     className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
       <div options={{ max:45, scale: 1, speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
@@ -18,7 +18,7 @@ const ServiceCard = ({index, title, icon}) => {
       </div>
 
     </motion.div>
-    </tilt>
+    </Tilt>
   )
 }
 
